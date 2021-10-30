5153841a90d0ff2438acdd0893514a9921933673
TakiKauser <takikauserbadhon@gmail.com>
2021-10-31 01:11:08 +0600

    delete-booking

--- index.js
+++ index.js
@@ -68,6 +68,14 @@ async function run() {
             res.send(result);
         });
 
+        // DELETE Booking
+        app.delete('/myBookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
         //   await client.close();
